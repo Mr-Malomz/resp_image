4edcd61327c499fb9a6c9e03dce2b2b1cb2314a0
demola malomo <malomo.alaba@yahoo.com>
2022-04-10 00:32:12 +0100

    responsive image with cloudinary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Cloudinary } from '@cloudinary/url-gen';
+import {
+  AdvancedImage,
+  responsive,
+  lazyload,
+  placeholder,
+  accessibility
+} from '@cloudinary/react';
 
 function App() {
+  //create cloudinary instance
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: process.env.REACT_APP_CLOUDINARY_CLOUD_NAME,
+    },
+  });
+
+  const myImage = cld.image('photo-1649486116313-d04e42c24590');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <h1>Responsive Image</h1>
       </header>
+        <AdvancedImage
+          cldImg={myImage}
+          plugins={[
+            lazyload(),
+            responsive({ steps: [200, 400] }),
+            accessibility(),
+            placeholder(),
+          ]}
+        />
     </div>
   );
 }
